ce42d0d45815d0d8e435ecb628fb23f880d0c3aa
陈杰勇 <295660782@qq.com>
2023-10-02 01:16:17 +0800

    single field

--- src/main/java/com/mytest/springboot/common/ResponseParams.java
+++ src/main/java/com/mytest/springboot/common/ResponseParams.java
@@ -12,53 +12,53 @@ import java.util.Map;
 import java.util.Set;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class ResponseParams<T> implements Serializable {
 
-    private static final long serialVersionUID=1L;
-    /**
-     * 状态码
-     *
-     * @required
-     */
-    private String code;
-    /**
-     * 返回信息
-     *
-     * @required
-     */
-    private String msg;
-
-    /**
-     * 属性
-     */
-    private List<Property> baseProperties = Collections.emptyList();
+//    private static final long serialVersionUID=1L;
+//    /**
+//     * 状态码
+//     *
+//     * @required
+//     */
+//    private String code;
+//    /**
+//     * 返回信息
+//     *
+//     * @required
+//     */
+//    private String msg;
+//
+//    /**
+//     * 属性
+//     */
+//    private List<Property> baseProperties = Collections.emptyList();
     /**
      * 返回对象
      */
     private T data;
 
-    /**
-     * 数组
-     */
-    private Set set;
+//    /**
+//     * 数组
+//     */
+//    private Set set;
+//
+//    /**
+//     * 图关系
+//     */
+//    private Map map;
 
-    /**
-     * 图关系
-     */
-    private Map map;
-
-    public ResponseParams(String code, String msg) {
-        this.code = code;
-        this.msg = msg;
-    }
-
-    public ResponseParams(T t) {
-        this.data = t;
-        this.code = "000";
-        this.msg = "success";
-    }
+//    public ResponseParams(String code, String msg) {
+//        this.code = code;
+//        this.msg = msg;
+//    }
+//
+//    public ResponseParams(T t) {
+//        this.data = t;
+//        this.code = "000";
+//        this.msg = "success";
+//    }
 
     @Data
     public static class Property {
