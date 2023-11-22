ea84882436ba7f413ffcde47f2b7ffce430c55c2
陈杰勇 <295660782@qq.com>
2023-11-22 11:49:36 +0800

    ax test file

---
+++ src/main/java/com/mytest/springboot/controller/anxin/AnXinTestController.java
@@ -0,0 +1,42 @@
+package com.mytest.springboot.controller.anxin;
+
+import com.eolinker.platform.api.response.PlainResult;
+import com.mytest.springboot.common.ResponseParams;
+import com.mytest.springboot.dto.NormalVO;
+import com.mytest.springboot.dto.SupermanDto;
+import com.mytest.springboot.dto.UserDto;
+import com.szbank.dto.Carda;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/anxin-test")
+public class AnXinTestController {
+
+    // carda方法
+    @PostMapping("/carda")
+    public Carda getCarda(@RequestBody List<SupermanDto> supermanDto) {
+        return null;
+    }
+
+    // hello方法
+    @GetMapping("/hello")
+    public ResponseParams<Integer> helloWorld(String name){
+        return null;
+    }
+
+    // cdelete方法
+    @DeleteMapping("/delete")
+    public PlainResult<Long> deleteUser(Integer userId, HttpServletRequest request){
+        return null;
+    }
+
+    // normal方法
+    @GetMapping("/normal")
+    public PlainResult<NormalVO> helloWorld(ResponseParams result, String name, UserDto userDto){
+        return null;
+    }
+
+}
