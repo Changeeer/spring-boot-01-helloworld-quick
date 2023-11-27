a74c13aa7af382c0426991ca1f5f4da0fdc3a93f
陈杰勇 <295660782@qq.com>
2023-11-27 15:31:38 +0800

    gbk file

--- src/main/java/com/mytest/springboot/controller/anxin/AnXinCharsetGBKTestController.java
+++ src/main/java/com/mytest/springboot/controller/anxin/AnXinCharsetGBKTestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/anxin-test")
-public class AnXinCharsetTestController {
+public class AnXinCharsetGBKTestController {
 
     /**
      * card·½·¨
