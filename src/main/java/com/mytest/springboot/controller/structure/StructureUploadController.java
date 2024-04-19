8ca716ae38a72154007b6abd06d577924a7a49b4
陈杰勇 <295660782@qq.com>
2024-04-19 11:17:31 +0800

    数据结构测试类 - 2

--- src/main/java/com/mytest/springboot/controller/structure/StructureUploadController.java
+++ src/main/java/com/mytest/springboot/controller/structure/StructureUploadController.java
@@ -4,24 +4,45 @@ import com.mytest.springboot.common.RequestParams;
 import com.mytest.springboot.common.ResponseParams;
 import com.mytest.springboot.dto.NormalVO;
 import com.mytest.springboot.dto.UserDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+/**
+ * @eo.api-type http
+ * @eo.groupName 数据结构测试
+ * @eo.path /structure
+ */
 
 @RestController
 @RequestMapping("/structure")
 public class StructureUploadController {
 
+    /**
+     * @eo.name get请求返回raw
+     * @eo.url /get-data
+     * @eo.method get
+     * @eo.request-type formdata
+     * @param id
+     * @param token
+     * @param getAllDetail
+     * @return String
+     */
     @GetMapping("/get-data")
     public String rawResp(Long id, String token, boolean getAllDetail) {
         return null;
     }
 
+    /**
+     * @eo.name post请求数据结构
+     * @eo.url /post-data
+     * @eo.method post
+     * @param req
+     * @return ResponseParams
+     * @eo.request-type json
+     */
     @PostMapping("/post-data")
-    public ResponseParams<NormalVO> getNormalByUser(RequestParams<List<UserDto>> req) {
+    public ResponseParams<NormalVO> getNormalByUser(@RequestBody RequestParams<List<UserDto>> req) {
         return null;
     }
 }
