abec881b42abcbe825ec9e0d0caaf033e2ab9d17
陈杰勇 <295660782@qq.com>
2024-05-29 14:46:15 +0800

    ds test - 12

--- src/main/java/com/mytest/springboot/controller/structure/StructureUploadController.java
+++ src/main/java/com/mytest/springboot/controller/structure/StructureUploadController.java
@@ -66,9 +66,9 @@ public class StructureUploadController
 
     @PostMapping("/post-data-address")
     public ResponseParams<Address> getAddress(
-            @RequestBody RequestParams<List<Address>> reqList,
-            String id,
-            RequestParams<Address> req,
+//            @RequestBody RequestParams<List<Address>> reqList,
+//            String id,
+//            RequestParams<Address> req,
             RequestParams<List<RequestParams<List<PageQuery>>>> reqListReqList
     ) {
         return null;
