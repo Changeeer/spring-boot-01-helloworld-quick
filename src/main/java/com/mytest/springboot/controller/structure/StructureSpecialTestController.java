26cf49e3f049969a7e9af0f1c17624645f2d4671
陈杰勇 <295660782@qq.com>
2024-05-30 10:59:02 +0800

    ds test - 17

---
+++ src/main/java/com/mytest/springboot/controller/structure/StructureSpecialTestController.java
@@ -0,0 +1,35 @@
+package com.mytest.springboot.controller.structure;
+
+import com.eolinker.platform.api.response.PlainResult;
+import com.mytest.springboot.common.ResponseParams;
+import com.mytest.springboot.dto.NormalVO;
+import com.mytest.springboot.dto.UserDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @eo.api-type http
+ * @eo.group-name 据结构测试.特殊测试
+ * @eo.path /structure-special-test
+ */
+
+@RestController
+@RequestMapping("/structure-special-test")
+public class StructureSpecialTestController {
+
+    /**
+     * @eo.name get-数据结构
+     * @eo.url /normal
+     * @eo.method get
+     * @eo.request-type formdata
+     * @param result
+     * @param name
+     * @param userDto
+     * @return PlainResult
+     */
+    @GetMapping("/normal")
+    public PlainResult<Boolean> helloWorld(ResponseParams result, String name, UserDto userDto){
+        return null;
+    }
+}
