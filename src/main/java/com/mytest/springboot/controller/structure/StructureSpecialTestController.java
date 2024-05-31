e16478b6318a5b2dc13e4b759b594a30d46a9f18
陈杰勇 <295660782@qq.com>
2024-05-31 15:11:04 +0800

    ds test - 20

--- src/main/java/com/mytest/springboot/controller/structure/StructureSpecialTestController.java
+++ src/main/java/com/mytest/springboot/controller/structure/StructureSpecialTestController.java
@@ -4,9 +4,8 @@ import com.eolinker.platform.api.response.PlainResult;
 import com.mytest.springboot.common.ResponseParams;
 import com.mytest.springboot.dto.NormalVO;
 import com.mytest.springboot.dto.UserDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mytest.springboot.dto.demo.Swagger2User;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @eo.api-type http
@@ -18,18 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/structure-special-test")
 public class StructureSpecialTestController {
 
+//    /**
+//     * @eo.name get-数据结构
+//     * @eo.url /normal
+//     * @eo.method get
+//     * @eo.request-type formdata
+//     * @param result
+//     * @param name
+//     * @param userDto
+//     * @return PlainResult
+//     */
+//    @GetMapping("/normal")
+//    public ResponseParams<Boolean> helloWorld(ResponseParams result, String name, UserDto userDto){
+//        return null;
+//    }
+
     /**
-     * @eo.name get-数据结构
-     * @eo.url /normal
-     * @eo.method get
-     * @eo.request-type formdata
-     * @param result
-     * @param name
-     * @param userDto
+     * @eo.name 第三方包
+     * @eo.url /test-group
+     * @eo.method post
+     * @eo.request-type json
+     * @param user
      * @return PlainResult
      */
-    @GetMapping("/normal")
-    public ResponseParams<Boolean> helloWorld(ResponseParams result, String name, UserDto userDto){
+    @PostMapping("/test-group")
+    public PlainResult<Long> s2TestGroup(@RequestBody PlainResult<UserDto> user) {
         return null;
     }
+
+
 }
